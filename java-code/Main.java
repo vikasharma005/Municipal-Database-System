7b3db0529c2d170e83a5f4a064691adbbefa404a
Vikas Sharma <93978146+vikasharma005@users.noreply.github.com>
2023-12-13 12:07:52 +0530

    Delete java-code directory

--- java-code/Main.java
+++
@@ -1,6 +0,0 @@
-package csw_endsem_project;
-public class Main {
-    public static void main(String a[]){
-    new Splashscreen(); // Constructor call for the splashcreen. Splashscreen then carry forwards the program.
-    }
-}
